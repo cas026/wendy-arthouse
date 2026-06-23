c3a4f67661ab905f85b47366529da934102bd40e
Cas <casvankempen60@gmail.com>
2026-06-23 21:20:30 +0200

    chore: add validation and mail dependencies, configure SMTP and Stripe

--- wa-backend/build.gradle.kts
+++ wa-backend/build.gradle.kts
@@ -22,6 +22,9 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-mail")
+	implementation("com.stripe:stripe-java:26.3.0")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("tools.jackson.module:jackson-module-kotlin")
 	runtimeOnly("com.mysql:mysql-connector-j")
